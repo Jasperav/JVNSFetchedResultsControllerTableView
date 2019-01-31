48b843226bad4a90b605d97d20908315c659d63e
Jasper <Jasperav@hotmail.com>
2019-01-31 20:10:57 +0100

    commit

--- JVNSFetchedResultsControllerTableView/Classes/NSFetchedResultsControllerViewController.swift
+++ JVNSFetchedResultsControllerTableView/Classes/NSFetchedResultsControllerViewController.swift
@@ -3,11 +3,11 @@ import JVGenericTableView
 import JVMiddleTextView
 import CoreData
 
-open class NSFetchedResultsControllerViewController<T: UITableViewCell, U: NSFetchRequestResult>: UIViewController, UITableViewDelegate {
+open class NSFetchedResultsControllerViewController<T: UITableViewCell, U: NSFetchRequestResult, Q: NSFetchedResultsControllerTableView<T, U>>: UIViewController, UITableViewDelegate {
     
     public var tapped: ((U) -> ())!
     public let tableView: GenericTableView<T>
-    public private (set) var nsFetchedResultsControllerTableView: NSFetchedResultsControllerTableView<T, U>!
+    public internal (set) var nsFetchedResultsControllerTableView: Q!
     
     public init(rowHeight: CGFloat, estimatedRowHeight: CGFloat, middleTextView: MiddleTextView, tableViewMode: NSFetchedResultsControllerTableView<T, U>.Mode, configure: ((_ cell: T, _ result: U) -> ())?, tapped: ((U) -> ())? = nil) {
         tableView = GenericTableView(cellType: T.self, style: .plain, rowHeight: rowHeight, estimatedRowHeight: estimatedRowHeight)
@@ -16,8 +16,6 @@ open class NSFetchedResultsControllerViewController<T: UITableViewCell, U: NSFet
         super.init(nibName: nil, bundle: nil)
         
         tableView.delegate = self
-        
-        nsFetchedResultsControllerTableView = NSFetchedResultsControllerTableView(tableView: tableView, view: view, middleTextView: middleTextView, resultController: type(of: self).createResultsController(), mode: tableViewMode, configure: configure)
     }
     
     public required init?(coder aDecoder: NSCoder) {
@@ -32,5 +30,35 @@ open class NSFetchedResultsControllerViewController<T: UITableViewCell, U: NSFet
         let object = nsFetchedResultsControllerTableView.getObject(indexPath: indexPath)
         
         tapped!(object)
+    }
+    
+    func createResultControllerDynamic() -> NSFetchedResultsController<U> {
+        return type(of: self).createResultsController()
+    }
+}
+
+
+open class NSFetchedResultsControllerViewControllerAutoScoll<T: UITableViewCell, U: NSFetchRequestResult> : NSFetchedResultsControllerViewController<T, U, NSFetchedResultsControllerTableViewAutoScroll<T, U>> {
+    public init(rowHeight: CGFloat, estimatedRowHeight: CGFloat, middleTextView: MiddleTextView, tableViewMode: NSFetchedResultsControllerTableView<T, U>.Mode, autoScrollWhenRowsAtBottomAreInserted: Bool, configure: ((_ cell: T, _ result: U) -> ())?, tapped: ((U) -> ())? = nil) {
+        super.init(rowHeight: rowHeight, estimatedRowHeight: estimatedRowHeight, middleTextView: middleTextView, tableViewMode: tableViewMode, configure: configure, tapped: tapped)
+        
+        nsFetchedResultsControllerTableView = NSFetchedResultsControllerTableViewAutoScroll(tableView: tableView, view: view, middleTextView: middleTextView, resultController: createResultControllerDynamic(), mode: tableViewMode, autoScrollWhenRowsAtBottomAreInserted: autoScrollWhenRowsAtBottomAreInserted, configure: configure)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+open class NSFetchedResultsControllerViewControllerNonScroll<T: UITableViewCell, U: NSFetchRequestResult> : NSFetchedResultsControllerViewController<T, U, NSFetchedResultsControllerTableView<T, U>> {
+    
+    public override init(rowHeight: CGFloat, estimatedRowHeight: CGFloat, middleTextView: MiddleTextView, tableViewMode: NSFetchedResultsControllerTableView<T, U>.Mode, configure: ((_ cell: T, _ result: U) -> ())?, tapped: ((U) -> ())? = nil) {
+        super.init(rowHeight: rowHeight, estimatedRowHeight: estimatedRowHeight, middleTextView: middleTextView, tableViewMode: tableViewMode, configure: configure, tapped: tapped)
+        
+        nsFetchedResultsControllerTableView = NSFetchedResultsControllerTableView(tableView: tableView, view: view, middleTextView: middleTextView, resultController: createResultControllerDynamic(), mode: tableViewMode, configure: configure)
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
