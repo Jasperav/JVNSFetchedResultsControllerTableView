7a8671b8096b727c9942dba6dc85cda750a0a60b
Jasper <Jasperav@hotmail.com>
2019-02-23 19:47:23 +0100

    commit

--- JVNSFetchedResultsControllerTableView/Classes/NSFetchedResultsControllerViewControllerAutoScoll.swift
+++ JVNSFetchedResultsControllerTableView/Classes/NSFetchedResultsControllerViewControllerAutoScoll.swift
@@ -2,14 +2,14 @@ import JVMiddleTextView
 import CoreData
 
 open class NSFetchedResultsControllerViewControllerAutoScoll<T: ConfigurableTableViewCell<U>, U: NSFetchRequestResult>: NSFetchedResultsControllerViewController<T, U, NSFetchedResultsControllerTableViewAutoScroll<T, U>> {
-    public init(middleTextView: MiddleTextView, tableViewMode: NSFetchedResultsControllerTableViewMode, autoScrollWhenRowsAtBottomAreInserted: Bool, loadPositionOffset: LoadCellOffset? = nil, configure: ((_ cell: T, _ result: U) -> ())?, resultController: NSFetchedResultsController<U>? = nil, tapped: ((U) -> ())? = nil) {
+    public init(middleTextView: MiddleTextView, tableViewMode: NSFetchedResultsControllerTableViewMode, autoScrollWhenRowsAtBottomAreInserted: Bool, loadPositionOffset: LoadCellOffset? = nil, configure: ((_ cell: T, _ result: U) -> ())? = nil, resultController: NSFetchedResultsController<U>? = nil, tapped: ((U) -> ())? = nil) {
         super.init(middleTextView: middleTextView, configure: configure, tapped: tapped)
         
         nsFetchedResultsControllerTableView = NSFetchedResultsControllerTableViewAutoScroll(tableView: tableView, view: view, middleTextView: middleTextView, resultController: resultController ?? createResultControllerDynamic(), mode: tableViewMode, loadPositionOffset: loadPositionOffset, autoScrollWhenRowsAtBottomAreInserted: autoScrollWhenRowsAtBottomAreInserted, configure: configure)
     }
     
     /// Use this when no query is needed to populate the screen
-    public convenience init(nothingSavedText: String, configure: ((_ cell: T, _ result: U) -> ())?, resultController: NSFetchedResultsController<U>? = nil, tapped: ((U) -> ())? = nil) {
+    public convenience init(nothingSavedText: String, configure: ((_ cell: T, _ result: U) -> ())? = nil, resultController: NSFetchedResultsController<U>? = nil, tapped: ((U) -> ())? = nil) {
         self.init(middleTextView: MiddleTextView(notQueryingText: nothingSavedText), tableViewMode: .notQuerying, autoScrollWhenRowsAtBottomAreInserted: false, configure: configure, tapped: tapped)
     }
     
